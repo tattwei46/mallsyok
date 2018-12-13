302cf00eb54cd8342701d41dcbea12f06b5a7f79
DavidCheah <tattwei46@gmail.com>
2018-12-13 19:55:07 +0800

    Added letter header

--- mallsyok/lib/screen/outlet_directory_screen.dart
+++ mallsyok/lib/screen/outlet_directory_screen.dart
@@ -7,6 +7,7 @@ import 'package:mallsyok/model/outlet.dart';
 import 'package:mallsyok/screen/select_mall_screen.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mallsyok/screen/outlet_details_screen.dart';
+import 'package:draggable_scrollbar/draggable_scrollbar.dart';
 
 enum Result { NOT_DETERMINED, FOUND, NOT_FOUND }
 
@@ -22,6 +23,7 @@ class OutletDirectoryPage extends StatefulWidget {
 class _OutletDirectoryPageState extends State<OutletDirectoryPage> {
   Result resultState = Result.NOT_DETERMINED;
   List<Outlet> _outletList = [];
+  String letterHead = "0";
 
   Widget _searchButton() {
     return IconButton(
@@ -57,9 +59,9 @@ class _OutletDirectoryPageState extends State<OutletDirectoryPage> {
     super.initState();
   }
 
-  Widget buildBody() {
+  Widget buildBody(double width) {
     if (resultState == Result.FOUND) {
-      return showResult();
+      return showResult(width);
     } else if (resultState == Result.NOT_FOUND) {
       return showNoResult();
     }
@@ -81,16 +83,44 @@ class _OutletDirectoryPageState extends State<OutletDirectoryPage> {
     );
   }
 
-  Widget showResult() {
+  Widget buildLetterHead(String letter, double width) {
+    return new Container(
+      width: width,
+      height: 40.0,
+      color: kColorPink,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 8.0, 0.0, 0.0),
+        child: Text(
+          letter,
+          style: new TextStyle(
+              fontSize: 20.0,
+              color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget showResult(double width) {
     return new Center(
         child: new ListView.builder(
-      itemCount: _outletList.length,
-      itemBuilder: (BuildContext context, int index) {
-        return OutletCard(
-          outlet: _outletList[index],
-        );
-      },
-    ));
+          itemCount: _outletList.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (_outletList[index].outletName.substring(0, 1) != letterHead) {
+              letterHead = _outletList[index].outletName.substring(0, 1);
+              return Column(
+                children: <Widget>[
+                  buildLetterHead(letterHead, width),
+                  OutletCard(
+                    outlet: _outletList[index],
+                  )
+                ],
+              );
+            }
+            return OutletCard(
+              outlet: _outletList[index],
+            );
+          },
+        ));
   }
 
   Widget showDrawer() {
@@ -166,6 +196,10 @@ class _OutletDirectoryPageState extends State<OutletDirectoryPage> {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery
+        .of(context)
+        .size
+        .width;
     return new Scaffold(
       appBar: new AppBar(
         backgroundColor: kColorPink,
@@ -177,7 +211,7 @@ class _OutletDirectoryPageState extends State<OutletDirectoryPage> {
           _searchButton(),
         ],
       ),
-      body: buildBody(),
+      body: buildBody(width),
       drawer: showDrawer(),
     );
   }
