addd77bc533c53745d9c029da42f42625d0f0cf0
DavidCheah <tattwei46@gmail.com>
2018-12-16 12:34:36 +0800

    Added show outlet by category

--- mallsyok/lib/screen/outlet_list_screen.dart
+++ mallsyok/lib/screen/outlet_list_screen.dart
@@ -7,6 +7,7 @@ import 'package:mallsyok/model/outlet.dart';
 import 'package:mallsyok/screen/outlet_details_screen.dart';
 import 'package:mallsyok/screen/search_outlet_screen.dart';
 import 'package:mallsyok/common_widget/platform_drawer.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 enum Result { NOT_DETERMINED, FOUND, NOT_FOUND }
 
@@ -22,6 +23,8 @@ class OutletListPage extends StatefulWidget {
 class _OutletListPageState extends State<OutletListPage> {
   Result resultState = Result.NOT_DETERMINED;
   List<Outlet> _outletList = [];
+  List<Outlet> _filteredOutletList = [];
+  List<Category> _categoryList = [];
   String letterHead = "0";
 
   Widget _searchButton() {
@@ -44,6 +47,10 @@ class _OutletListPageState extends State<OutletListPage> {
     if (event.documents.length > 0) {
       _outletList.addAll(
           event.documents.map((snapshot) => Outlet.fromSnapshot(snapshot)));
+
+      // Make a working copy for filtered outletlist
+      _filteredOutletList = new List<Outlet>.from(_outletList);
+
       setState(() {
         resultState = Result.FOUND;
       });
@@ -60,7 +67,14 @@ class _OutletListPageState extends State<OutletListPage> {
   void initState() {
     // Reset result state
     resultState = Result.NOT_DETERMINED;
-    // Get mall list
+
+    // Create category list
+    createCategoryList();
+
+    // Set All Category as selected
+    _categoryList[0].isTapped = true;
+
+    // Get outlet list
     ServiceOutlet().getOutletList(widget.mall.key, _onEntryAdded);
 
     super.initState();
@@ -84,6 +98,21 @@ class _OutletListPageState extends State<OutletListPage> {
     );
   }
 
+  void showOutletByCategory(String category) {
+    _filteredOutletList.clear();
+
+    if (category == AppConfig.TEXT_CATEGORY_ALL){
+      _filteredOutletList = new List<Outlet>.from(_outletList);
+    } else {
+      for (int i = 0; i < _outletList.length; i++) {
+        if (_outletList[i].category == category) {
+          _filteredOutletList.add(_outletList[i]);
+        }
+      }
+    }
+
+  }
+
   Widget showLoading() {
     return new Center(
       child: CircularProgressIndicator(),
@@ -105,14 +134,15 @@ class _OutletListPageState extends State<OutletListPage> {
     );
   }
 
-  Widget showResult(double width) {
+  Widget buildListView(double width) {
     String firstLetter;
     return new Center(
         child: new ListView.builder(
-      itemCount: _outletList.length,
+      itemCount: _filteredOutletList.length,
       itemBuilder: (BuildContext context, int index) {
         firstLetter =
-            _outletList[index].outletName.substring(0, 1).toUpperCase();
+            _filteredOutletList[index].outletName.substring(0, 1).toUpperCase();
+
         if (double.tryParse(firstLetter) != null) {
           firstLetter = "#";
         }
@@ -122,21 +152,196 @@ class _OutletListPageState extends State<OutletListPage> {
             children: <Widget>[
               buildLetterHead(letterHead, width),
               OutletCard(
-                outlet: _outletList[index],
+                outlet: _filteredOutletList[index],
               )
             ],
           );
         }
         return OutletCard(
-          outlet: _outletList[index],
+          outlet: _filteredOutletList[index],
         );
       },
     ));
   }
 
+  Widget showResult(double width) {
+    return Center(
+      child: _filteredOutletList.length > 0 ? buildListView(width): showNoResult(),
+    );
+  }
+
+  Widget buildCategory(double width, double headerHeight, double bodyHeight) {
+    return Column(
+      children: <Widget>[
+        //buildCategoryHeader(width, headerHeight),
+        buildCategoryBody(bodyHeight),
+      ],
+    );
+  }
+
+  Icon buildCategoryIcon(IconData icons) {
+    return Icon(
+      icons,
+      color: kAppThemeColor,
+      size: 50.0,
+    );
+  }
+
+  void createCategoryList() {
+    //All Category
+    _categoryList.add(Category(AppConfig.TEXT_CATEGORY_ALL, Icons.apps));
+
+    // Departmental Store
+    _categoryList.add(Category(
+        AppConfig.TEXT_CATEGORY_DEPARTMENT, Icons.local_grocery_store));
+
+    // Beauty & Hair Salon
+    _categoryList
+        .add(Category(AppConfig.TEXT_CATEGORY_BEAUTY, FontAwesomeIcons.cut));
+
+    // Personal Care & Pharmacy
+    _categoryList.add(
+        Category(AppConfig.TEXT_CATEGORY_PHARMACY, FontAwesomeIcons.firstAid));
+
+    // Fitness & Wellness Centre
+    // Health & Fitness Equipment
+    _categoryList.add(
+        Category(AppConfig.TEXT_CATEGORY_FITNESS, FontAwesomeIcons.dumbbell));
+
+    // Cosmetics & Skincare
+    _categoryList.add(Category(AppConfig.TEXT_CATEGORY_COSMETICS, Icons.face));
+
+    // Digital Lifestyle
+    _categoryList
+        .add(Category(AppConfig.TEXT_CATEGORY_DIGITAL, Icons.desktop_mac));
+
+    // Entertainment
+    _categoryList
+        .add(Category(AppConfig.TEXT_CATEGORY_ENTERTAINMENT, Icons.movie));
+    // Fashion
+    _categoryList.add(
+        Category(AppConfig.TEXT_CATEGORY_FASHION, FontAwesomeIcons.shoePrints));
+
+    // Financial Service & ATM
+    _categoryList
+        .add(Category(AppConfig.TEXT_CATEGORY_ATM, FontAwesomeIcons.piggyBank));
+
+    // Food & Beverages
+    _categoryList.add(Category(AppConfig.TEXT_CATEGORY_FOOD, Icons.fastfood));
+
+    // Home & Living
+    _categoryList
+        .add(Category(AppConfig.TEXT_CATEGORY_HOME, FontAwesomeIcons.wrench));
+
+    // Convenience & Services
+    _categoryList.add(Category(
+        AppConfig.TEXT_CATEGORY_CONVENIENCE, Icons.local_convenience_store));
+
+    // Optical
+    _categoryList.add(
+        Category(AppConfig.TEXT_CATEGORY_OPTICS, FontAwesomeIcons.glasses));
+
+    // Timepieces & Jewelleries
+    _categoryList.add(
+        Category(AppConfig.TEXT_CATEGORY_JEWELLERIES, FontAwesomeIcons.ring));
+
+    // Books & Stationaries
+    _categoryList
+        .add(Category(AppConfig.TEXT_CATEGORY_BOOK, FontAwesomeIcons.book));
+
+    // Toys & Hobbies
+    _categoryList.add(Category(AppConfig.TEXT_CATEGORY_TOYS, Icons.toys));
+
+    // Music & Instruments
+    _categoryList
+        .add(Category(AppConfig.TEXT_CATEGORY_MUSIC, Icons.music_note));
+
+    // Gifts & Souvenirs
+    _categoryList
+        .add(Category(AppConfig.TEXT_CATEGORY_GIFTS, Icons.card_giftcard));
+
+    // Office
+    _categoryList.add(
+        Category(AppConfig.TEXT_CATEGORY_OFFICE, FontAwesomeIcons.briefcase));
+  }
+
+  void resetAllCategory() {
+    for (int i = 0; i < _categoryList.length; i++) {
+      _categoryList[i].isTapped = false;
+    }
+  }
+
+  Widget buildCategoryBody(double height) {
+    double iconHeight = 60.0;
+    bool isTapped;
+    return new Container(
+      height: height,
+      child: new ListView.builder(
+        scrollDirection: Axis.horizontal,
+        physics: ClampingScrollPhysics(),
+        itemCount: _categoryList.length,
+        itemBuilder: (BuildContext context, int index) {
+          isTapped = _categoryList[index].isTapped;
+          return GestureDetector(
+            child: Container(
+              color: Colors.white,
+              width: height,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: iconHeight,
+                    child: Icon(
+                      _categoryList[index].iconData,
+                      color: isTapped ? kAppTextColor : Colors.black45,
+                      size: 30.0,
+                    ),
+                  ),
+                  Container(
+                    height: height - iconHeight,
+                    child: new Text(
+                      _categoryList[index].title,
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(
+                          fontSize: 12.0,
+                          color: isTapped ? kAppTextColor : Colors.black45),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            onTap: () {
+              setState(() {
+                resetAllCategory();
+                _categoryList[index].isTapped = !_categoryList[index].isTapped;
+                showOutletByCategory(_categoryList[index].title);
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildCategoryHeader(double width, double headerHeight) {
+    return new Container(
+      width: width,
+      height: 40.0,
+      color: kAppThemeColor,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 8.0, 0.0, 0.0),
+        child: Text(
+          "Category",
+          style: new TextStyle(fontSize: 20.0, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
+    double categoryBodyHeight = 100.0;
+    double categoryHeaderHeight = 40.0;
     return new Scaffold(
       appBar: new AppBar(
         backgroundColor: kColorPink,
@@ -147,6 +352,10 @@ class _OutletListPageState extends State<OutletListPage> {
         actions: <Widget>[
           _searchButton(),
         ],
+        bottom: PreferredSize(
+            child:
+                buildCategory(width, categoryHeaderHeight, categoryBodyHeight),
+            preferredSize: Size(0.0, categoryBodyHeight)),
       ),
       body: buildBody(width),
       drawer: PlatformDrawer(
@@ -203,4 +412,12 @@ class OutletCard extends StatelessWidget {
           ],
         ));
   }
+}
+
+class Category {
+  String title;
+  IconData iconData;
+  bool isTapped = false;
+
+  Category(this.title, this.iconData);
 }
